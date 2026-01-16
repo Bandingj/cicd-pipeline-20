4949027992637ae424a7831454e4951825a6a472
bandingj <bandingjawara@gmail.com>
2026-01-16 16:16:04 +0100

    Initial Terraform project (clean)

---
+++ main.tf
@@ -0,0 +1,26 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "my-bucket-neny554654646"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0ecb62995f68bb549"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "my-server1111"
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "my-vpc1"
+  }
+}
